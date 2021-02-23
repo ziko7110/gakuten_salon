ab976848693ece86b903d3b4c9a7193e5c75684d
ziko7110 <ec2-user@ip-172-31-28-192.ap-northeast-1.compute.internal>
2021-02-23 01:12:15 +0000

    テスト

--- app/Http/Middleware/RedirectToHttps.php
+++ app/Http/Middleware/RedirectToHttps.php
@@ -15,7 +15,9 @@ class RedirectToHttps
      */
     public function handle($request, Closure $next)
     {
-        //このhandleメソッドで判別
+        
+        
+        
         if(!$this->is_ssl() && config('app.env') === 'production'){
             return redirect('https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
         }
